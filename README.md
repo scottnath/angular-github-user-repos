228296de4804bb9b1fe1e5ee06daf8b7e559731d
Scott Nath <github@scottnath.com>
2015-01-14 15:18:25 -0500

    alpha version complete

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# angular-github-user-repos
+Angular GitHub User Repositories
+=====================
+
 Simple AngularJS module to show a user's public GitHub repositories
+
+## What it does
+
+Creates a basic list of a user's GitHub *public* repositories which mimics the styling of GitHubs list of repositories.
+
+## Usage
+
+### Dependency in your app
+
+You will need to include ```angularGithubUserRepos``` in your list of app dependencies:
+
+```
+'use strict';
+
+angular.module('angularApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngRoute', 'angularGithubUserRepos'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+	  	...
+```
+
+### Using the directive in your html
+
+```<githubs user="scottnath" limit="15"></githubs>```
+
+### Styling
+
+You must include both the octocons.css (bower_components/octicons/octicons/octicons.css) and the angular-github-user-repos.css files for this to look correct and include all icons.
+
+## Options
+
+### user
+
+Type: `String`
+
+This should be a valid GitHub username. *Module will fail without this attribute.*
+
+Default: *none*
+
+### limit
+
+Type: `Num`
+
+This option allows you to limit the number of results returned.
+
+Default: `10`
---
+++ angular-github-user-repos.js
@@ -0,0 +1,103 @@
+'use strict';
+
+/*!
+ * AngularJS GitHub User Repositories
+ * https://github.com/scottnath/angular-picasa-album
+ * @license MIT
+ * v0.0.1
+ */
+
+(function(){
+
+angular.module('angularGithubUserRepos', ['angular-github-user-repos/user-repos.html', 'angularMoment'])
+  .directive('githubs', ['githubService', function(githubService) {
+    return {
+      //works on attribute
+      restrict: 'E',
+      replace: true,
+      scope: { 
+        user: '@',
+        limit: '@'
+      },
+      templateUrl: 'angular-github-user-repos/user-repos.html',
+      link: function(scope, element, attrs) {
+
+        scope.$watch('user', function () {
+          if (angular.isUndefined(scope.user)) {
+          	console.log('no user given');
+            scope.githuberror = "No GitHub user provided";
+            return;
+          }
+          if (angular.isUndefined(scope.limit) || !angular.isNumber(scope.limit)) {
+            scope.limit = 10;
+            console.log(scope.limit);
+          }
+          githubService.get(scope.user,scope.limit).then(function(data) {
+            scope.repos = data;
+            scope.ready = true;
+          })
+        });
+        
+      }
+    };
+  }])
+  .factory('githubService', ['$http', '$q', function($http, $q) {
+    // Service logic
+
+    $http.defaults.useXDomain = true;
+    
+    function loadUser(user,limit) {
+      var d = $q.defer();
+      $http.jsonp('https://api.github.com/users/' + user + '/repos?page=1&per_page=' + limit + '&callback=JSON_CALLBACK').success(function(data, status) {
+        d.resolve(data);
+      });
+      return d.promise;
+    }
+
+    // Public API here
+    return {
+      get : function (user,limit) {
+        return loadUser(user,limit);
+      }
+    };
+  }]);
+
+
+  angular.module("angular-github-user-repos/user-repos.html", []).run(["$templateCache", function($templateCache) {
+    var userReposTemplate="";
+    userReposTemplate += "<div class=\"container github-user-repos\">";
+    userReposTemplate += "  <span class=\"error\" ng-show=\"repo.githuberror\">{{githuberror}}<\/span>";
+    userReposTemplate += "  <ul class=\"repo-list\">";
+    userReposTemplate += "    <li class=\"repo-list-item\" ng-repeat=\"repo in repos.data | orderBy:'updated_at':true\">";
+    userReposTemplate += "      <div class=\"repo-list-stats\">";
+    userReposTemplate += "        <a class=\"repo-list-stat-item tooltipped tooltipped-s stargazers\" href=\"{{repo.svn_url}}\/stargazers\" aria-label=\"Stargazers\">";
+    userReposTemplate += "          <span class=\"octicon octicon-star\"><\/span>";
+    userReposTemplate += "          {{repo.stargazers_count}}";
+    userReposTemplate += "        <\/a>";
+    userReposTemplate += "        <a class=\"repo-list-stat-item tooltipped tooltipped-s forks\" href=\"{{repo.svn_url}}\/network\" aria-label=\"Forks\">";
+    userReposTemplate += "          <span class=\"octicon octicon-git-branch\"><\/span>";
+    userReposTemplate += "          {{repo.forks}}";
+    userReposTemplate += "        <\/a>";
+    userReposTemplate += "        <a class=\"repo-list-stat-item tooltipped tooltipped-s watchers\" href=\"{{repo.svn_url}}\/network\" aria-label=\"Forks\">";
+    userReposTemplate += "          <span class=\"octicon octicon-eye\"><\/span>";
+    userReposTemplate += "          {{repo.watchers}}";
+    userReposTemplate += "        <\/a>";
+    userReposTemplate += "      <\/div>";
+    userReposTemplate += "      <h3 class=\"repo-list-name\">";
+    userReposTemplate += "        <a href=\"{{repo.svn_url}}\">{{repo.name}}<\/a>";
+    userReposTemplate += "      <\/h3>";
+    userReposTemplate += "      <p class=\"repo-list-description\" ng-show=\"repo.description\">{{repo.description}}<\/p>";
+    userReposTemplate += "";
+    userReposTemplate += "      <p class=\"repo-list-meta\">";
+    userReposTemplate += "          Updated <time datetime=\"{{repo.updated_at}}\" is=\"relative-time\" title=\"{{repo.updated_at | amDateFormat:'dddd, MMMM Do YYYY, h:mm:ss a'}}\">{{repo.updated_at | amTimeAgo}}<\/time>";
+    userReposTemplate += "      <\/p>";
+    userReposTemplate += "";
+    userReposTemplate += "    <\/li>";
+    userReposTemplate += "  <\/ul>";
+    userReposTemplate += "";
+    userReposTemplate += "<\/div>";
+    userReposTemplate += "";    
+    $templateCache.put("angular-github-user-repos/user-repos.html",userReposTemplate);
+  }]);
+
+})();
